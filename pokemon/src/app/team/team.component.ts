1d8cfb74a15181a1a1af8d5d3a4d092b0676a856
Aliany Crespo <alianycres@gmail.com>
2021-02-28 22:06:22 +0100

    Style team and trainer

--- pokemon/src/app/team/team.component.ts
+++ pokemon/src/app/team/team.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
 
   pokemonList: string[] = ["picachu"];
+  trainerList: string[] = ["Ana", "Juan", "Pepe"]
 
   constructor() { }
 
